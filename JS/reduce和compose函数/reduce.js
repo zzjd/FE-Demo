876eeb234e510ebb5a8e02e0d6b99b9607a230e5
Zhijie Zhou <zhijie.zhou@shopee.com>
2021-12-23 17:52:33 +0800

    修改文件 架构 和 reduce 函数

--- JS/reduce和compose函数/reduce.js
+++ JS/reduce和compose函数/reduce.js
@@ -13,14 +13,12 @@ Array.prototype.myReduce = function (callback, prev) {
   // this 代表这个源数组
   for (let i = 0; i < this.length; i++) {
     if (prev === undefined) {
-      prev = callback(this[i], this[i + 1], i + 1, this);
+      prev = callback(this[i], this[i + 1]);
       i++;
     } else {
-      prev = callback(prev, this[i], i, this);
+      prev = callback(prev, this[i]);
     }
-    console.log("prevprevprev", prev);
   }
-  console.log("callbackcallbackcallback", callback);
   return prev;
 };
 
@@ -32,14 +30,12 @@ result = arr.myReduce((N, item) => {
   // 第三次: 60 40
   // ...
   // reduce 只传递一个回调函数，那么N第一次默认是第一项，后续的N是上一次函数执行的处理结果
-  console.log(N, item);
   return N + item;
 }, 100);
 console.log(result);
 
 //第二个例子
-// result = arr.myreduce((N, item) => {
-//   console.log(N, item);
-//   return N + item;
-// }, 0); //=>REDUCE的第二个参数就是给N赋值的初始值 ITEM从数组第一项开始遍历
-// console.log(result);
+result = arr.myReduce((N, item) => {
+  return N + item;
+}, 0); //=>REDUCE的第二个参数就是给N赋值的初始值 ITEM从数组第一项开始遍历
+console.log(result);
